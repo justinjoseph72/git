2877c689726ca62a0e2e5a07d235dcc316c85110
Justin Joseph <justin.joseph72@outlook.com>
2014-07-27 18:54:46 +0530

    makeing use of ModelDriven and ActionSupport

---
+++ HelloWorldStruts2/src/formBeans/HelloBean.java
@@ -0,0 +1,37 @@
+/*------------------------------------------------------
+File Name-----HelloBean.java
+Author------justin
+Date created--------27-Jul-2014 -1:08:43 pm
+-------------------------------------------------------*/
+
+/**
+ * 
+ */
+package formBeans;
+
+/**
+ * @author justin
+ * 
+ */
+public class HelloBean
+{
+private String guestName;
+
+/**
+ * @return the guestName
+ */
+public String getGuestName()
+{
+	return guestName;
+}
+
+/**
+ * @param guestName the guestName to set
+ */
+public void setGuestName(String guestName)
+{
+	this.guestName = guestName;
+}
+
+
+}
--- HelloWorldStruts2/src/hello/Hello.java
+++ HelloWorldStruts2/src/hello/Hello.java
@@ -1,20 +1,63 @@
 package hello;
 
-public class Hello 
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+import formBeans.HelloBean;
+
+public class Hello extends ActionSupport implements ModelDriven<HelloBean>
 {
-	private String guestName;
+	HelloBean myBean = new HelloBean();
 
-	public String getGuestName() {
-		return guestName;
-	}
-
-	public void setGuestName(String guestName) {
-		this.guestName = guestName;
-	}
-	//execute method
+	// execute method
 	public String execute()
 	{
 		return "success";
+	}
+
+	/*
+	 * (non-Javadoc)retuns a new model object which is a formbean of HelloBean
+	 */
+	@Override
+	public HelloBean getModel()
+	{
+		// TODO Auto-generated method stub
+		return myBean;
+	}
+
+	/**
+	 * @return the myBean
+	 */
+	public HelloBean getMyBean()
+	{
+		return myBean;
+	}
+
+	/**
+	 * @param myBean
+	 *            the myBean to set
+	 */
+	public void setMyBean(HelloBean myBean)
+	{
+		this.myBean = myBean;
+	}
+
+	/*
+	 * (non-Javadoc) gets the object of model and validates the inputs before
+	 * executing the execute method
+	 */
+	@Override
+	public void validate()
+	{
+		if(this.getMyBean()== null)
+		{
+			addFieldError("guestName", "Name :Please Provide a Name and resubmit");
+		}
+		if(this.getMyBean().getGuestName().equals(""))
+		{
+			//addActionError("Name :Please Provide a Name and resubmit");
+			addFieldError("guestName", "Name :Please Provide a Name and resubmit");
+		}
 	}
 
 }
